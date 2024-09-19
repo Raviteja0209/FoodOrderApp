2161f44956b37e8b76738ab882ecf47dc4c0acea
Raviteja0209 <raviteja.2998@gmail.com>
2024-09-19 17:50:02 +0530

    added swiggy api

--- src/Utils/data.js
+++ src/Utils/data.js
@@ -1,83 +1,22 @@
-import hotel1 from "/src/Images/hotel1.jpg";
-import hotel2 from "/src/Images/hotel2.jpg";
-import hotel3 from "/src/Images/hotel3.jpg";
-import hotel4 from "/src/Images/hotel4.jpg";
-import hotel5 from "/src/Images/hotel5.jpg";
-import hotel6 from "/src/Images/hotel6.jpg";
-import hotel7 from "/src/Images/hotel7.jpg";
-import hotel8 from "/src/Images/hotel8.jpg";
-
+const IMG_CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+const restinfo = [];
 export const fetchInfo = async () => {
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.335031&lng=78.4833637&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.3525114&lng=78.4773256&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
     const response = await data.json();
+    let restaurants = response?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+    restaurants.map((restaurants) => (
+        restinfo.push({
+            "id" : restaurants.info.id,
+            "restimg" : IMG_CDN_URL+restaurants.info.cloudinaryImageId,
+            "restname" : restaurants.info.name,
+            "cuisine" : restaurants.info.cuisines.join(","),
+            "rating" : restaurants.info.avgRating,
+            "deltime" : restaurants.info.sla.slaString
+        })
+    ))
     return restinfo;
 }
 
-const restinfo  =  [
-    {
-        "id" : 1,
-        "restimg" : hotel1,
-        "restname" : "Mehfil",
-        "cuisine" : "Biryani,Chinese,Kebabs,Tandoor,Chandrayangutta",
-        "rating" : "4.3",
-        "deltime" : "25-30 mins"
-    },
-    {
-        "id" : 2,
-        "restimg" : hotel2,
-        "restname" : "Hotel Rumaan",
-        "cuisine" : "Hyderabadi,Biriyani,Charminar",
-        "rating" : "3.5",
-        "deltime" : "30-35 mins"
-    },
-    {
-        "id" : 3,
-        "restimg" : hotel3,
-        "restname" : "Imperial Multicuisine",
-        "cuisine" : "Biryani,Chinese,Kebabs,Tandoor,Chandrayangutta",
-        "rating" : "4.3",
-        "deltime" : "25-30 mins"
-    },
-    {
-        "id" : 4,
-        "restimg" : hotel4,
-        "restname" : "Shah Ghouse Hotel & Restaurant",
-        "cuisine" : "Biryani,Chinese,Kebabs,Tandoor,Chandrayangutta",
-        "rating" : "4.3",
-        "deltime" : "25-30 mins"
-    },
-    {
-        "id" : 5,
-        "restimg" : hotel5,
-        "restname" : "Bawarchi",
-        "cuisine" : "Biryani,Chinese,Kebabs,Tandoor,Chandrayangutta",
-        "rating" : "4.3",
-        "deltime" : "25-30 mins"
-    },
-    {
-        "id" : 6,
-        "restimg" : hotel6,
-        "restname" : "Rumaan Restaurant",
-        "cuisine" : "Biryani,Chinese,Kebabs,Tandoor,Chandrayangutta",
-        "rating" : "3.8",
-        "deltime" : "25-30 mins"
-    },
-    {
-        "id" : 7,
-        "restimg" : hotel7,
-        "restname" : "Santosh Dhaba",
-        "cuisine" : "Biryani,Chinese,Kebabs,Tandoor,Chandrayangutta",
-        "rating" : "3.9",
-        "deltime" : "25-30 mins"
-    },
-    {
-        "id" : 8,
-        "restimg" : hotel8,
-        "restname" : "Shadaan Restaurant",
-        "cuisine" : "Biryani,Chinese,Kebabs,Tandoor,Chandrayangutta",
-        "rating" : "4.3",
-        "deltime" : "25-30 mins"
-    },
-]
 
 export default restinfo;
